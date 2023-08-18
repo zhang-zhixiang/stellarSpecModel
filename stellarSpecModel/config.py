c99d0b94f6cafb1ce932512b4629dbacd8793bf8
zzx <zzx@bigtree.xyz>
2023-08-18 14:15:48 +0800

    update the grid file info: filename, url, md5

--- stellarSpecModel/config.py
+++ stellarSpecModel/config.py
@@ -1,9 +1,10 @@
 import os
 
-
-grid_data_dir = '~/.stellarSpecModel/grid_data/'
+home_dir = os.path.expanduser('~')
+grid_data_dir = '{}/.stellarSpecModel/grid_data/'.format(home_dir)
 
 grid_names = {
-    'BTCond': 'BTCond_grid.hdf5',
-    'MARCS': 'MARCS_grid.hdf5'
+    # grid_name: (file_name, url, md5)
+    'MARCS': ('MARCS_grid.hdf5', 'https://www.jianguoyun.com/p/DZmcNoUQ2ZfcCBjW-5cFIAA', 'ee32ad18eb0ab427bcebeffb0b4916ce'),
+    'BTCond': 'BTCond_grid.hdf5', 'https://www.jianguoyun.com/p/DZmcNoUQ2ZfcCBjW-5cFIAA', 'ee32ad18eb0ab427bcebeffb0b4916ce'),
 }
