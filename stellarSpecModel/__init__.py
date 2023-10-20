8e59577debcd76fd4655886607b2279d5e7c4d01
zzx <zzx@bigtree.xyz>
2023-10-20 17:58:45 +0800

    add the MARCS_grid_hiRes to init file

--- stellarSpecModel/__init__.py
+++ stellarSpecModel/__init__.py
@@ -2,6 +2,7 @@ from . import config
 from .stellarSpecModel import StellarSpecModel
 from .stellarSpecModel import MARCS_Model
 from .stellarSpecModel import BTCond_Model
+from .stellarSpecModel import MARCS_Model_hiRes
 
 
 __version__ = '0.1.0'
